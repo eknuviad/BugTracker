1f963242a5fac4a511652f4f3ca38a37b86348c3
QuintusHe <zirui.he@mail.mcgill.ca>
2020-05-05 14:56:27 -0400

    remove test for now

--- BugTracker-Backend/src/test/java/ca/mcgill/ecse321/bugtracker/BugtrackerApplicationTests.java
+++ BugTracker-Backend/src/test/java/ca/mcgill/ecse321/bugtracker/BugtrackerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BugtrackerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
