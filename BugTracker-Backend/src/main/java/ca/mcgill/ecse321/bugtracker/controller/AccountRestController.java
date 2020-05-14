ef61a551c05f7bbbe86aae9a8558fe7eb230fccb
QuintusHe <zirui.he@mail.mcgill.ca>
2020-05-13 22:25:03 -0400

    add accEmail to userRoleDTO

--- BugTracker-Backend/src/main/java/ca/mcgill/ecse321/bugtracker/controller/AccountRestController.java
+++ BugTracker-Backend/src/main/java/ca/mcgill/ecse321/bugtracker/controller/AccountRestController.java
@@ -13,6 +13,7 @@ import ca.mcgill.ecse321.bugtracker.model.UserRole;
 @RestController
 public class AccountRestController {
     
+    
 
 
 
--- BugTracker-Backend/src/main/java/ca/mcgill/ecse321/bugtracker/dto/UserRoleDTO.java
+++ BugTracker-Backend/src/main/java/ca/mcgill/ecse321/bugtracker/dto/UserRoleDTO.java
@@ -7,19 +7,17 @@ import ca.mcgill.ecse321.bugtracker.model.Account;
 public class UserRoleDTO {
 
     private String userName;
+    private String email;
 
     //UserRole Associations
-    private Account acc;
-    private List<ProjectDTO> projects;
-    private List<InvitationDTO> invitations;
-    private List<TicketDTO> tickets;
+    //private Account acc;
+    // private List<ProjectDTO> projects;
+    // private List<InvitationDTO> invitations;
+    // private List<TicketDTO> tickets;
 
-    public UserRoleDTO (String userName, Account acc, List<ProjectDTO> projects, List<InvitationDTO> invitations, List<TicketDTO> tickets){
+    public UserRoleDTO (String userName, String email){
         this.userName = userName;
-        this.acc = acc;
-        this.projects = projects;
-        this.invitations = invitations;
-        this.tickets = tickets;
+        this.email = email;
     }
   
     
