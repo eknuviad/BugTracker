a338f8eaed1b8c6aed49fa75477a3c6de2cd2306
Edem Nuviadenu <edemnuviadenu@gmail.com>
2020-05-13 23:16:58 -0400

    createDeveloper,getaccountbyemail, updaterole implemented

--- BugTracker-Backend/src/main/java/ca/mcgill/ecse321/bugtracker/controller/AccountRestController.java
+++ BugTracker-Backend/src/main/java/ca/mcgill/ecse321/bugtracker/controller/AccountRestController.java
@@ -3,7 +3,11 @@ package ca.mcgill.ecse321.bugtracker.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.mcgill.ecse321.bugtracker.dto.AccountDTO;
@@ -12,12 +16,43 @@ import ca.mcgill.ecse321.bugtracker.model.Account;
 import ca.mcgill.ecse321.bugtracker.model.Admin;
 import ca.mcgill.ecse321.bugtracker.model.Manager;
 import ca.mcgill.ecse321.bugtracker.model.UserRole;
+import ca.mcgill.ecse321.bugtracker.service.AccountService;
+import ca.mcgill.ecse321.bugtracker.service.UserRoleService;
 
 @CrossOrigin(origins = "*")
 @RestController
 public class AccountRestController {
     
-    
+    @Autowired
+    private AccountService aService;
+
+    @Autowired
+    private UserRoleService uService;
+
+
+    @PostMapping(value = {"/addrole/developer/", "/addrole/developer"})
+    public AccountDTO createDeveloperByAccount(@RequestParam("username") String username, @RequestParam("password") String password, @RequestParam("email") String email)throws IllegalArgumentException {
+        Account ac = aService.getAccountByEmail(email);
+        uService.createDeveloperRoleByAccount(password, username, ac);
+        List<UserRole> ur = uService.getAllUserRolesByAccount(ac);
+        return convertToDTO(ac, ur);
+    }
+
+    @GetMapping(value = {"/account/email/", "/account/email"})
+    public AccountDTO getAccountByEmail(@RequestParam("email") String email)throws IllegalArgumentException {
+        Account ac = aService.getAccountByEmail(email);
+        List<UserRole> ur = uService.getAllUserRolesByAccount(ac);
+        return convertToDTO(ac, ur);
+    }
+
+    @PostMapping(value = {"/update/role/", "/update/role"})
+    public AccountDTO updateUserRole(@RequestParam("username") String username, @RequestParam("password") String password, @RequestParam("email") String email)throws IllegalArgumentException {
+        Account ac = aService.getAccountByEmail(email);
+        UserRole ur = uService.getUserRoleByUserName(username);
+        uService.updateUserRole(password, username, ur);
+        List<UserRole> urList = uService.getAllUserRolesByAccount(ac);
+        return convertToDTO(ac, urList);
+    }
 
 
 
