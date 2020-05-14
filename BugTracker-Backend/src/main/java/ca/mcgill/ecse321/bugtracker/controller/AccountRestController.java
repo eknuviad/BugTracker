ca8c0e6750c033c83a3194db9a4924b1cfa28477
QuintusHe <zirui.he@mail.mcgill.ca>
2020-05-13 23:46:32 -0400

    add createManager method

--- BugTracker-Backend/src/main/java/ca/mcgill/ecse321/bugtracker/controller/AccountRestController.java
+++ BugTracker-Backend/src/main/java/ca/mcgill/ecse321/bugtracker/controller/AccountRestController.java
@@ -1,34 +1,67 @@
 package ca.mcgill.ecse321.bugtracker.controller;
 
+import java.rmi.AccessException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import ca.mcgill.ecse321.bugtracker.dao.AccountRepository;
 import ca.mcgill.ecse321.bugtracker.dto.AccountDTO;
 import ca.mcgill.ecse321.bugtracker.dto.UserRoleDTO;
 import ca.mcgill.ecse321.bugtracker.model.Account;
 import ca.mcgill.ecse321.bugtracker.model.Admin;
 import ca.mcgill.ecse321.bugtracker.model.Manager;
 import ca.mcgill.ecse321.bugtracker.model.UserRole;
+import ca.mcgill.ecse321.bugtracker.service.AccountService;
+import ca.mcgill.ecse321.bugtracker.service.UserRoleService;
 
 @CrossOrigin(origins = "*")
 @RestController
 public class AccountRestController {
+
+    @Autowired 
+    private AccountService accountService;
+
+    @Autowired
+    private UserRoleService urService;
+
     
-    @PostMapping("/create/account")
-    public AccountDTO postAccount(@RequestParam("accName") String name,
+    @PostMapping({"/create/account", "/create/account/"})
+    public AccountDTO createAccount(@RequestParam("accName") String name,
             @RequestParam("accEmail") String email,
             @RequestParam("description") String description,
-            @RequestParam("phoneNum") int phoneNumber,
-            @RequestParam("userRoles") UserRoleDTO roleDto){
+            @RequestParam("phoneNum") int phoneNumber){
         
-        return null;
+        Account account = accountService.createAccount(name, email, description, phoneNumber);
+        List<UserRole> userRoles = urService.getAllUserRolesByAccount(account);
+        AccountDTO accountDto = convertToDTO(account, userRoles);
+        return accountDto;
     }
+
+    @PostMapping({"/addrole/manager", "/addrole/manager/"})
+    public AccountDTO createManagerRole(@RequestParam("userName") String userName,
+            @RequestParam("password") String password,
+            @RequestParam("email") String email) {
+        
+        Account account = accountService.getAccountByEmail(email);
+        if (account == null){
+            throw new IllegalArgumentException("Account dosen't exists.");
+        }
+        Manager manager = urService.createManagerRoleByAccount(password, userName, account);
+        List<UserRole> roles = urService.getAllUserRolesByAccount(account);
+        AccountDTO accountDto = convertToDTO(account, roles);
+        return accountDto;
+        
+    }
+
+
+
 
 
 
