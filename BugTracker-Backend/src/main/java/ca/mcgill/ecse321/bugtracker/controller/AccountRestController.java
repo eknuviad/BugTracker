b472c189a6cec7139cf04f917ed9e7674fe83e7c
Edem Nuviadenu <edemnuviadenu@gmail.com>
2020-05-13 22:36:32 -0400

    Added converttodtos

--- BugTracker-Backend/src/main/java/ca/mcgill/ecse321/bugtracker/controller/AccountRestController.java
+++ BugTracker-Backend/src/main/java/ca/mcgill/ecse321/bugtracker/controller/AccountRestController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import ca.mcgill.ecse321.bugtracker.dto.AccountDTO;
 import ca.mcgill.ecse321.bugtracker.model.Account;
+import ca.mcgill.ecse321.bugtracker.model.Admin;
+import ca.mcgill.ecse321.bugtracker.model.Manager;
 import ca.mcgill.ecse321.bugtracker.model.UserRole;
 
 @CrossOrigin(origins = "*")
@@ -22,10 +24,32 @@ public class AccountRestController {
         if (account == null) {
             throw new IllegalArgumentException("There is no account.");
         }
-        // List<UserRoleDTO> urDTO = converttoDTO(userrole);
-        // AccountDTO accDTO = new AccountDTO(account.getName(),account.getEmail(), account.getDescription(), account.getPhoneNumber(),urDTO);
-        // return accDTO;
-        return null;
+        
+        List<UserRoleDTO> urDTO = convertToDTO(userrole);
+        for(UserRole u : userrole){
+            urDTO.add(convertToDTO(u));
+        }
+        AccountDTO accDTO = new AccountDTO(account.getName(),account.getEmail(), account.getDescription(), account.getPhoneNumber(),urDTO);
+        return accDTO;
     
+    }
+
+    private UserRoleDTO convertToDTO(UserRole ur){
+        if (ur == null) {
+            throw new IllegalArgumentException("There is no userrole.");
+        }
+        UserRoleDTO urDTO;
+        String username;
+        if(ur instanceof Manager){
+            username = "Manager-" + ur.getUserName();
+        }else if (ur instanceof Admin){
+             username = "Admin-" + ur.getUserName();
+        }else{[
+            username = "Developer-" + ur.getUserName();
+           
+        }
+        urDTO = new UserRoleDTO (username, ur.getUser().getEmail());
+
+        return urDTO;
     }
 }
