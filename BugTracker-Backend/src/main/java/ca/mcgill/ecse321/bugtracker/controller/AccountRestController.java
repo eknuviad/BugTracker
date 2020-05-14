eaa12f607e17878b4e84957ea5c871d52a5bcd6d
QuintusHe <zirui.he@mail.mcgill.ca>
2020-05-13 22:47:07 -0400

    lol

--- BugTracker-Backend/src/main/java/ca/mcgill/ecse321/bugtracker/controller/AccountRestController.java
+++ BugTracker-Backend/src/main/java/ca/mcgill/ecse321/bugtracker/controller/AccountRestController.java
@@ -3,9 +3,13 @@ package ca.mcgill.ecse321.bugtracker.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.mcgill.ecse321.bugtracker.dto.AccountDTO;
+import ca.mcgill.ecse321.bugtracker.dto.UserRoleDTO;
 import ca.mcgill.ecse321.bugtracker.model.Account;
 import ca.mcgill.ecse321.bugtracker.model.UserRole;
 
@@ -13,8 +17,15 @@ import ca.mcgill.ecse321.bugtracker.model.UserRole;
 @RestController
 public class AccountRestController {
     
-    
-
+    @PostMapping("/create/account")
+    public AccountDTO postAccount(@RequestParam("accName") String name,
+            @RequestParam("accEmail") String email,
+            @RequestParam("description") String description,
+            @RequestParam("phoneNum") int phoneNumber,
+            @RequestParam("userRoles") UserRoleDTO roleDto){
+        
+        return null;
+    }
 
 
 
