7961b00a59d4b3b4ef69f2da56648e17232fbf19
QuintusHe <zirui.he@mail.mcgill.ca>
2020-05-18 15:15:26 -0400

    add invitation dto

--- BugTracker-Backend/src/main/java/ca/mcgill/ecse321/bugtracker/dto/InvitationDTO.java
+++ BugTracker-Backend/src/main/java/ca/mcgill/ecse321/bugtracker/dto/InvitationDTO.java
@@ -1,5 +1,45 @@
 package ca.mcgill.ecse321.bugtracker.dto;
 
+import ca.mcgill.ecse321.bugtracker.model.UserRole;
+import ca.mcgill.ecse321.bugtracker.model.Invitation.InvitationStatus;
+import ca.mcgill.ecse321.bugtracker.service.InvitationService;
+
 public class InvitationDTO {
+
+  private InvitationStatus invStatus;
+  private int id;
+  //Invitation Associations
+  private UserRoleDTO userRole;
+  //private Project project;
+
+  public InvitationDTO (InvitationStatus status, UserRoleDTO role, int id){
+      this.invStatus = status;
+      this.userRole = role;
+      this.id = id;
+  }
+
+  public InvitationStatus getInvStatus() {
+      return invStatus;
+  }
+
+  public void setInvStatus(InvitationStatus invStatus) {
+      this.invStatus = invStatus;
+  }
+
+  public int getId() {
+      return id;
+  }
+
+  public void setId(int id) {
+      this.id = id;
+  }
+
+  public UserRoleDTO getUserRole() {
+      return userRole;
+  }
+
+  public void setUserRole(UserRoleDTO userRole) {
+      this.userRole = userRole;
+  }
     
 }
