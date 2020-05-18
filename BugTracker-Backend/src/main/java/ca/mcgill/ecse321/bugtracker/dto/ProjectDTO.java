4530e87958c6622c96dfeb5338ca91b8e42b33b4
QuintusHe <zirui.he@mail.mcgill.ca>
2020-05-18 15:11:57 -0400

    add project and ticket dto

--- BugTracker-Backend/src/main/java/ca/mcgill/ecse321/bugtracker/dto/ProjectDTO.java
+++ BugTracker-Backend/src/main/java/ca/mcgill/ecse321/bugtracker/dto/ProjectDTO.java
@@ -1,5 +1,67 @@
 package ca.mcgill.ecse321.bugtracker.dto;
 
+import java.util.List;
+
+import org.omg.CORBA.portable.IDLEntity;
+
+import ca.mcgill.ecse321.bugtracker.model.Invitation;
+import ca.mcgill.ecse321.bugtracker.model.Ticket;
+
 public class ProjectDTO {
-    
+    private String name;
+    private int id;
+  
+    //Project Associations
+    private UserRoleDTO userRole;
+    private List<InvitationDTO> invitations;
+    private List<TicketDTO> tickets;
+
+    public ProjectDTO (String name, int id, UserRoleDTO role, 
+                        List<InvitationDTO> invitations, List<TicketDTO> tickets){
+        this.name = name;
+        this.id = id;
+        this.userRole = role;
+        this.invitations = invitations;
+        this.tickets = tickets;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public UserRoleDTO getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(UserRoleDTO userRole) {
+        this.userRole = userRole;
+    }
+
+    public List<InvitationDTO> getInvitations() {
+        return invitations;
+    }
+
+    public void setInvitations(List<InvitationDTO> invitations) {
+        this.invitations = invitations;
+    }
+
+    public List<TicketDTO> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<TicketDTO> tickets) {
+        this.tickets = tickets;
+    }
 }
--- BugTracker-Backend/src/main/java/ca/mcgill/ecse321/bugtracker/dto/TicketDTO.java
+++ BugTracker-Backend/src/main/java/ca/mcgill/ecse321/bugtracker/dto/TicketDTO.java
@@ -1,6 +1,68 @@
 
 package ca.mcgill.ecse321.bugtracker.dto;
 
+import java.sql.Date;
+
+import ca.mcgill.ecse321.bugtracker.model.Project.TicketStatus;
+
 public class TicketDTO {
+
+    private TicketStatus ticketStatus;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+  
+    //Ticket Associations
+    // private UserRole userRole;
+    // private Project p;
+    private int id;
+
+    public TicketDTO (TicketStatus status, String description, Date startDate, Date endDate, int id){
+        this.ticketStatus = status;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.id = id;
+    }
+
+    public TicketStatus getTicketStatus() {
+        return ticketStatus;
+    }
+
+    public void setTicketStatus(TicketStatus ticketStatus) {
+        this.ticketStatus = ticketStatus;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
     
 }
